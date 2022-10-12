db4be62b4e009bc07c7a5cfda2dae12a86b4e62b
Krishna Chaitanya Sornapudi <107940081+Krrish95@users.noreply.github.com>
2022-10-12 12:26:04 +0530

    Update problem03.js

--- Day_01/problem03.js
+++ Day_01/problem03.js
@@ -1,3 +1,6 @@
+// Problem 3 ; Create a variable name to store your name. Create a variable age to store your age. 
+// Print the values stored in the variable on one line followed by the type of the variable in the next line.
+
 let name = "Krishna Chaitanya"; 
 let age = 25; 
 
