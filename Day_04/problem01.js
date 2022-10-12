db88d7a3f49cbbc1d83119532ba787f811efa3c5
Krishna Chaitanya Sornapudi <107940081+Krrish95@users.noreply.github.com>
2022-10-12 12:39:33 +0530

    Update problem01.js

--- Day_04/problem01.js
+++ Day_04/problem01.js
@@ -1,3 +1,6 @@
+// Problem 1: Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+
+
 let yob = 1996; 
 let age = 2022-yob; 
 
