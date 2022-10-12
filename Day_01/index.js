9006da0c2dcc1fd279dd0a4c2f278f569fd3627e
Krishna Chaitanya Sornapudi <107940081+Krrish95@users.noreply.github.com>
2022-10-12 12:18:23 +0530

    Update index.js

--- Day_01/index.js
+++ Day_01/index.js
@@ -1,3 +1,6 @@
+// Problem01 : Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+
 let x = "Masai School"; 
 let y = "A Transformation in Education"; 
 
